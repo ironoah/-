ea90122357460459db8e7eacfa6b145c54ddb56e
ntani <ironoah@gmail.com>
2015-10-02 00:45:38 +0900

    implement method 各勘定を締め切る()

--- src/決算.java
+++ src/決算.java
@@ -6,7 +6,17 @@ class 決算 {
 	public void 当期純利益を資本金勘定に振り替える() {
 	}
 	
-	public void 各勘定を締め切る() {
+	public boolean 各勘定を締め切る() {
+		if (売上.借方.損益 != 売上.貸方.現金 +  売上.貸方.売掛金) {
+			return false;
+		}
+		if (損益.借方.仕入 + 損益.借方.資本金 != 損益.貸方.売上) {
+			return false;
+		}
+		if (仕入.借方.現金 + 仕入.借方.買掛金 != 仕入.貸方.損益) {
+			return false;
+		}
+		return true;
 	}
 	
 	public void 繰越試算表を作成する() {
