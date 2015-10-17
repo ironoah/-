d161de8477becb66e6115867d3e756cec5481ddd
Nat <ironoah@gmail.com>
2015-10-17 23:27:22 +0900

    Update BracQuest.java

--- src/BracQuest.java
+++ src/BracQuest.java
@@ -1,4 +1,11 @@
 class BracQuest {
   public static void main(String[] args) {
+    Brave b = new Brave();
+    Map   m = new Map();
+    m.setHuman(b);
+    for ( int cnt=0; cnt<10; cnt++) {
+      b.randomWalk();
+    }
+    
   }
 }
